d79ba84d2c3f2fb0f34bf2ff77616bdb11e77ee9
hkpark2030 <hkpark2030@users.noreply.github.com>
2015-12-03 22:43:27 -0500

    Create start-node-js-sample.sh

---
+++ start-node-js-sample.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Clone assignment repository and call the scripts 
+#git clone https://github.com/hkpark2030/deploy-node-js-sample.git
+#cd deploy-node-js-sample
+
+gnome-terminal -x sh -c "bash deploy-node-js.sh"
+sleep 5m
+gnome-terminal -x sh -c "bash deploy-heroku.sh"
+sleep 2m
+gnome-terminal -x sh -c "bash deploy-firewall-rules.sh"
